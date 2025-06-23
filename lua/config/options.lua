49d090abe48e548760ba9859779588ca4772918a
Matt Milunski <matt.milunski@slaom.com>
2025-06-22 20:57:46 -0500

    Add column at 80

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- Highlight the 80th and 100th columns
+vim.opt.colorcolumn = "80"
+vim.cmd [[highlight ColorColumn ctermbg=lightgrey guibg=lightgrey]]
